2a5fb8a7a2eb0e531bb2e89ef6f206e896ffbe2a
KanishkaArochana <135253502+KanishkaArochana@users.noreply.github.com>
2023-07-31 20:52:30 +0530

    Add files via upload

---
+++ Lab_Day_05/Question 01/script.js
@@ -0,0 +1,16 @@
+// Qustion 01
+// alert("This is a alert!");
+
+var mark;
+for(var x=0;x<5;x++){
+ var mark = prompt("Enter the marks:");
+  
+}
+var sum=0; 
+for(var x=0;x<5;x++){
+       sum=sum+mark[x];
+   }
+
+var avg=sum/5;
+console.log(+avg);
+// alert("Avarage Marks :" +avg);

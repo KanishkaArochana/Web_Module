dd99a86f6457b2dbede6de17292f4874c9bb905a
KanishkaArochana <135253502+KanishkaArochana@users.noreply.github.com>
2023-07-31 20:54:56 +0530

    Add files via upload

---
+++ Lab_Day_08/index.php
@@ -0,0 +1,82 @@
+<?php
+//    Qustion 01
+// $x=6;
+// $y=4;
+
+// $sum=$x + $y;
+// echo("Sum is :" .$sum);
+// echo("<br>");
+
+// $dif=$x - $y;
+// echo("Difference is :" .$dif);
+// echo("<br>");
+
+// $pro=$x * $y;
+// echo("Product is :" .$pro);
+// echo("<br>");
+
+// $div=$x / $y;
+// echo("Division is :" .$div);
+// echo("<br>");
+
+
+//Qustion 02
+// for($x=5;$x<15;$x++){
+   
+// }
+// echo("Count is :" .$x);
+
+
+//Qustion 03
+
+// $unit=21;
+
+// if(0<= $unit && $unit<=50){
+//     echo("Your Payment is :" .$unit*3.50);
+// }
+
+// else if(51<= $unit && $unit<=100){
+//     echo("Your Payment is :" .$unit*4.00);
+// }
+
+// else if(101<= $unit && $unit<=150){
+//     echo("Your Payment is :" .$unit*5.20);
+// }
+
+// else{
+//     echo("Your Payment is :" .$unit*6.50);
+// }
+
+//Qustion 04
+
+// $num=3;
+
+// switch($num){
+//     case 1: echo("Monday");
+//     break;
+
+//     case 2: echo("Tuesday");
+//     break;
+
+//     case 3: echo("Wednesday");
+//     break;
+
+//     case 4: echo("Thursday");
+//     break;
+
+//     case 5: echo("Friday");
+//     break;
+
+//     case 6: echo("Saturday");
+//     break;
+
+//     case 7: echo("Sunday");
+//     break;
+
+//     default :echo("Invalid Number");
+// }
+
+//Qustion 05
+
+
+?>
